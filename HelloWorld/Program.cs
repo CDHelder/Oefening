656a55c4da421e99640b9d8592ab54bb0e03daf7
CDHelder <72396857+CDHelder@users.noreply.github.com>
2020-10-08 14:28:19 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Chris big giveaway");
+			Console.Write("Choose a door: 1, 2 or 3: ");
+			string userValue = Console.ReadLine();
+
+			if (userValue == "1")
+			{
+				string message = "You won a car!";
+				Console.WriteLine(message);
+			}
+			else if (userValue == "2")
+			{
+				string message = "You won a boat!";
+				Console.WriteLine(message)
+			}
+			else if (userValue == "3")
+			{
+				string message = "You won a scooter";
+				Console.WriteLine(message)
+			}
+			else
+			{
+				string message = "Please Enter 1, 2 or 3";
+				Console.WriteLine(message);
+			}
+			Console.ReadLine();
+		}	
+	}
+}
